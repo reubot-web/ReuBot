bdc140c13f714233926f08005933af739311d6a7
reubot-web <basafranluiza@gmail.com>
2026-02-22 23:13:53 +0800

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,5 +1,4 @@
-const PORT = process.env.PORT || 3000;
-
+app.use(express.static(path.join(__dirname, "public")));
 io.on('connection', (socket) => {
   console.log('a user connected');
 
@@ -11,6 +10,8 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 });
+
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
