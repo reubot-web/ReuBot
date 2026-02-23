91b9a00299a6a9c71f07f0b8dc3dd1f200a569c5
reubot-web <basafranluiza@gmail.com>
2026-02-23 20:25:39 +0800

    Update server.js

--- public/server.js
+++ public/server.js
@@ -148,5 +148,16 @@ io.on("connection", (socket) => {
   }
 });
 
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`✅ ReuBot running on port ${PORT}`));
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
